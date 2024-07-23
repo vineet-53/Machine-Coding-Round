44fe8940e393e6ce16dc36a8ce09d3db0e7070f9
vineet-53 <vineetsinghnegi53@gmail.com>
2024-07-23 17:08:36 +0530

    added custom data for pages

---
+++ src/data/courses.data.js
@@ -0,0 +1,32 @@
+export const courses = {
+  title: "Manage Bundle",
+  subTitle: "Change orders of the products based on priority",
+  data: [
+    {
+      img: "../assets/images/courses/1.png",
+      title: "Interview preparation with JavaScript 2.0",
+      price: "9000",
+      category: "course",
+    },
+    {
+      img: "../assets/images/courses/2.png",
+      title: "Aptitude - Averages, Mixtures & Allegation",
+      category: "Mock Test",
+    },
+    {
+      img: "../assets/images/courses/3.png",
+      title: "Aptitude - Simple & Compound Interest",
+      category: "Mock Test",
+    },
+    {
+      img: "../assets/images/courses/4.png",
+      title: "Aptitude - Partnership",
+      category: "Mock Test",
+    },
+    {
+      img: "../assets/images/courses/5.png",
+      title: "Aptitude - Time & Work",
+      category: "Mock Test",
+    },
+  ],
+};
---
+++ src/data/table.data.js
@@ -0,0 +1,62 @@
+export const batches = {
+  heading: "Chai aur Code",
+  title: "Batches",
+  subTitle: "Create learner’s batch and share information at the same time.",
+  data: [
+    {
+      img: "",
+      title: "",
+      date: {
+        start: "",
+        end: "",
+      },
+      price: "",
+      validity: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      date: {
+        start: "",
+        end: "",
+      },
+      price: "",
+      validity: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      date: {
+        start: "",
+        end: "",
+      },
+      price: "",
+      validity: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      date: {
+        start: "",
+        end: "",
+      },
+      price: "",
+      validity: "",
+      status: "",
+    },
+    {
+      img: "",
+      title: "",
+      date: {
+        start: "",
+        end: "",
+      },
+      price: "",
+      validity: "",
+      status: "",
+    },
+  ],
+};
